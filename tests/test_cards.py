3de10701c4ed936628fc8131caf3fe9c1f389209
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-11-26 15:11:21 +0900

    [add] Deck class test

--- tests/test_cards.py
+++ tests/test_cards.py
@@ -10,3 +10,29 @@ def my_card():
 
 def test_card(my_card):
     assert repr(my_card) == 'D-A'
+
+
+@pytest.fixture
+def my_deck():
+    deck = cards.Deck()
+    deck.shuffle()
+    return deck
+
+
+def draw(my_deck, num):
+    draw_cards = []
+    for _ in range(num):
+        draw_cards.append(repr(my_deck.draw()))
+
+    return draw_cards
+
+
+def test_deck(my_deck):
+    num = len(cards.Card.SUITS) * len(cards.Card.RANKS)
+
+    draw_cards1 = draw(my_deck, num)
+    my_deck.shuffle()
+    draw_cards2 = draw(my_deck, num)
+
+    assert len(set(draw_cards1)) == num
+    assert draw_cards2 != draw_cards1
