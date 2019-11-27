c8e12716941291ff74fafc7f0c882e41b1604d34
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-11-27 14:18:40 +0900

    [rename] VALUES ---> _VALUES

--- blackjack_python/cards.py
+++ blackjack_python/cards.py
@@ -6,14 +6,14 @@ class Card:
     """
     :type SUITS: tuple[str]
     :type RANKS: tuple[str]
-    :type VALUES: list[int]
+    :type _VALUES: list[int]
     :type suit: str
     :type rank: str
     :type value: int
     """
     SUITS = ('S', 'C', 'D', 'H')
     RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
-    VALUES = [min(i + 1, 10) for i in range(len(RANKS))]
+    _VALUES = [min(i + 1, 10) for i in range(len(RANKS))]
 
     def __init__(self, suit, rank):
         """
@@ -22,7 +22,7 @@ class Card:
         """
         self.suit = suit
         self.rank = rank
-        self.value = Card.VALUES[Card.RANKS.index(rank)]
+        self.value = Card._VALUES[Card.RANKS.index(rank)]
 
     def __repr__(self):
         return f"{self.suit}-{self.rank}"
