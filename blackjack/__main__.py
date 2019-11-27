f2d88f161ebc7790cbb6a0114479f9385f690087
Daisuke Kurosawa <dai-kuro@sings.jp>
2019-11-27 13:30:42 +0900

    [add] BlackJack class

--- blackjack/__main__.py
+++ blackjack/__main__.py
@@ -15,3 +15,66 @@
 - ダブルダウンなし、スプリットなし、サレンダーなし、その他特殊そうなルールなし
 - ゲームを何ゲームも続けられるようにする
 """
+from .cards import Deck
+from .players import Dealer
+from .players import Player
+
+
+def main():
+    game = BlackJack()
+    game.play()
+
+
+class BlackJack:
+    """
+    :type deck: Deck
+    :type player: Player
+    :type dealer: Dealer
+    """
+
+    def __init__(self):
+        self.deck = Deck()
+        self.player = Player()
+        self.dealer = Dealer()
+
+    def play(self):
+        print('\n<----------- Deal ----------->')
+        self.dealer.deal(self.deck, self.player)
+        print('<---------------------------->\n')
+
+        self.player.hit(self.deck)
+
+        if self.player.score <= Player.MAX_SCORE:
+            self.dealer.hit(self.deck)
+
+        print('<---------------------------->')
+        self.player.show()
+        self.dealer.show()
+        print('<---------------------------->')
+
+        print(f"\n{self.judge()}\n")
+
+    def judge(self):
+        """
+        :rtype: str
+        """
+        if self.player.score > Player.MAX_SCORE:
+            return 'Dealer Win'
+
+        if self.dealer.score > Dealer.MAX_SCORE:
+            return 'Player Win'
+
+        if self.player == self.dealer:
+            return 'Draw'
+
+        if self.player > self.dealer:
+            return 'Player Win'
+
+        if self.player < self.dealer:
+            return 'Dealer Win'
+
+        raise ValueError
+
+
+if __name__ == '__main__':
+    main()
